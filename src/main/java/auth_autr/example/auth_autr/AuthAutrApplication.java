d038bdda16537d1d818121add012929886384608
Mohamed <mohame@gmail.com>
2024-10-18 14:59:42 +0100

    premier commit

---
+++ src/main/java/auth_autr/example/auth_autr/AuthAutrApplication.java
@@ -0,0 +1,13 @@
+package auth_autr.example.auth_autr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAutrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAutrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auth_autr/example/auth_autr/AuthAutrApplicationTests.java
@@ -0,0 +1,13 @@
+package auth_autr.example.auth_autr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAutrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
