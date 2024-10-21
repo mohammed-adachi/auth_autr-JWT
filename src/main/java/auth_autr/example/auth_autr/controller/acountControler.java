8e96c7fa00ca973eec48c763eddf5d00289834f9
Mohamed <mohame@gmail.com>
2024-10-21 17:38:10 +0100

    feat: Add user registration endpoint with JWT token generation

--- src/main/java/auth_autr/example/auth_autr/controller/acountControler.java
+++ src/main/java/auth_autr/example/auth_autr/controller/acountControler.java
@@ -1,33 +1,86 @@
 package auth_autr.example.auth_autr.controller;
 
 import java.time.Instant;
+import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
+import auth_autr.example.auth_autr.models.registerDLO;
 import auth_autr.example.auth_autr.models.user;
+import auth_autr.example.auth_autr.repositry.userrepositry;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/acount")
 public class acountControler {
-  @Value("${security.jwt.token.secret-key}")
+  @Value("${security.jwt.secret-key}")
   private String secretKey;
   @Value("${security.jwt.issuer}")
     private String issuer;
-    private createJwtToken( user user){
+ @Autowired
+ private userrepositry userrepositry; 
+
+  @PostMapping("/register")
+  public ResponseEntity<Object> register(@ Valid @RequestBody registerDLO registerDLO ,BindingResult result){
+    if(result.hasErrors()){
+      var  errorlist= result.getAllErrors();
+      var errorMap= new HashMap<String,String>();
+      for(var error:errorlist){
+        errorMap.put(error.getObjectName(),error.getDefaultMessage());
+      }
+      return ResponseEntity.badRequest().body(errorMap);
+    }
+    
+   var bCryptPassword= new BCryptPasswordEncoder();
+    var user= new user();
+
+    user.setUsername(registerDLO.getUsername());
+    user.setPassword(bCryptPassword.encode(registerDLO.getPassword()));
+    user.setEmail(registerDLO.getEmail());
+    user.setRole("ROLE_USER");
+   
+    try {
+      var otheruser= userrepositry.findByUsername(registerDLO.getUsername());
+      if(otheruser != null){
+        return ResponseEntity.badRequest().body("user already exist");
+      }
+
+      var otheruseremail= userrepositry.findByEmail(registerDLO.getEmail());
+      if(otheruseremail != null){
+        return ResponseEntity.badRequest().body("email already exist");
+      }
+      userrepositry.save(user);
+      
+      var response= new HashMap<String,Object>();
+      response.put("user",user);
+      response.put("token",createJwtToken(user));
+      return ResponseEntity.ok(response);
+
+    } catch (Exception e) {
+      System.out.println("is ana exception");
+      e.printStackTrace();
+    }
+    return ResponseEntity.badRequest().body("error");
+  }
+
+    private String createJwtToken(user user){
     Instant now = Instant.now();
     JwtClaimsSet claims = JwtClaimsSet.builder()
     .subject(user.getUsername())
@@ -39,7 +92,7 @@ public class acountControler {
     .build();
     var encoder= new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
     var parms= JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(),claims);
-     return encoder.encode(parms);  
+     return encoder.encode(parms).getTokenValue();  
     }
 
 }
